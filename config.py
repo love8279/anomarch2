6537699025efb2b7253ecf84ddbfab65d7469317
love8279 <lovekushchoudhary315@gmail.com>
2026-03-28 21:21:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hadh"
+api_id = "20417561"
+api_hash = "c01124688b006ef00fa6ac926d6cb3e9"
 bot_token = "bot token"
-auth_users = [7890781002]
-sudo_users = [7890781002]
+auth_users = [6938716674]
+sudo_users = [6938716674]
